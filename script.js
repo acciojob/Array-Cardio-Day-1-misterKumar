e59ec17e95206c34dfff7e4c3da9cec3b7cd5a45
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-28 19:45:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -28,9 +28,15 @@ const people = [
 
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's and return the filtered array
-export function myfilter() {
-
+function myfilter() {
+	const fifteen = inventors.filter(inventor => {
+		if(inventor.year >= 1500 && inventor.year<1600){
+			console.log(inventor)
+			return inventor;
+		}
+	})
 }
+myfilter()
 
 // Array.prototype.map()
 // 2. Give us an array of the inventor first and last names (i.e. full name)
